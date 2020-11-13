8aa4191a991d6b03055d5d01a02b5ef81801bf43
ProjectHBK <34718421+projecthbk@users.noreply.github.com>
2020-11-13 15:37:33 +0100

    Update verify.php

--- verify.php
+++ verify.php
@@ -2,12 +2,12 @@
 	require_once('config.php');
 	$qrcode=$_POST['code'];
 	if (explode(':',$qrcode)[0]=='AMDeposit' && strlen($_POST['pin'])==4) {
-		$codes = unserialize(file_get_contents($path . '/AMDused.txt'));
+		$codes = unserialize(file_get_contents($path . '/AMDused'));
 		$value = explode(':',$qrcode)[1];
 		$code = explode(':',$qrcode)[2]; 
 		if (!in_array($code,$codes) && $code==md5($value . strftime('%Y-%m',time()) . $_POST['pin'] . $secret)) {
 			$codes[]=$code;
-			file_put_contents($path . '/AMDused.txt',serialize($codes));
+			file_put_contents($path . '/AMDused',serialize($codes));
 			echo 'PAY: ' . $value;
 		} else echo 'FAILURE';
 	} else echo 'ERROR';
