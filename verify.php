2309d26546e1006b048230afc7380a63022da866
ProjectHBK <34718421+projecthbk@users.noreply.github.com>
2020-11-17 23:43:03 +0100

    Update verify.php

--- verify.php
+++ verify.php
@@ -2,13 +2,13 @@
 	require_once('config.php');
 	$qrcode=$_POST['code'];
 	if (explode(':',$qrcode)[0]=='AMDeposit' && strlen($_POST['pin'])==4) {
-		$codes = unserialize(file_get_contents($path . '/AMDwithdraw'));
+		$codes = unserialize(file_get_contents($path . '/AMD.db'));
 		$value = explode(':',$qrcode)[1];
 		$code = explode(':',$qrcode)[2];
 		$id =  explode(':',$qrcode)[3];
 		if (!in_array($code . ':' . $id,$codes) && $code==hash('sha256',$value . strftime('%Y-%m',time()) . $_POST['pin'] . $id . $secret)) {
 			$codes[]=$code . ':' . $id;
-			file_put_contents($path . '/AMDwithdraw',serialize($codes));
+			file_put_contents($path . '/AMD.db',serialize($codes));
 			echo 'PAY: ' . $value;
 		} else echo 'FAILURE';
 	} else echo 'ERROR';
