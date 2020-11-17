ccf04f63b295c478e64562927dd0523f6f1793f8
ProjectHBK <34718421+projecthbk@users.noreply.github.com>
2020-11-18 00:55:54 +0100

    Update config.php

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
-	//path to secure folder not public on web server (with non readable but executable parent folder)
+	//path to secure folder (not public on web server)
 	$path = '/home/pi/74d4ddea0d225d5a'; 
 	//security protection string (individual to your deposit)
 	$secret = 'bb88a4dd51973147d62225718034c9d6';
