065201192c5ae398cc851aaf4aaaafc4b2c02ccb
ProjectHBK <34718421+projecthbk@users.noreply.github.com>
2020-11-17 22:59:53 +0100

    Update generate.php

--- generate.php
+++ generate.php
@@ -2,7 +2,7 @@
 	require_once('config.php');
 	require_once('phpqrcode/qrlib.php');
 	$codes = unserialize(file_get_contents($path . '/AMDnew'));
-	$code = md5(number_format($_POST['value'],2) . strftime('%Y-%m',strtotime($_POST['date'])) . $_POST['pin'] . $secret);
+	$code = hash('sha256',number_format($_POST['value'],2) . strftime('%Y-%m',strtotime($_POST['date'])) . $_POST['pin'] . $secret);
 	if (!in_array($code,$codes) && strlen($_POST['pin'])==4 && $_POST['value']>0 && $_POST['date']!='') {
 		$codes[]=$code;
 		file_put_contents($path . '/AMDnew',serialize($codes));
