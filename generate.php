c068ca27f9c1a0216abddaa3221805afc4c80a10
ProjectHBK <34718421+projecthbk@users.noreply.github.com>
2020-11-18 00:33:54 +0100

    Update generate.php

--- generate.php
+++ generate.php
@@ -1,8 +1,8 @@
 <?php
 	require_once('config.php');
 	require_once('phpqrcode/qrlib.php');
-	$id = bin2hex(openssl_random_pseudo_bytes(12));
-	$code = hash('sha256',number_format($_POST['value'],2) . strftime('%Y-%m',strtotime($_POST['date'])) . $_POST['pin'] . $id . $secret);
+	$hash = bin2hex(openssl_random_pseudo_bytes(12));
+	$code = hash('sha256',number_format($_POST['value'],2) . strftime('%Y-%m',strtotime($_POST['date'])) . $_POST['pin'] . $hash . $secret);
 	if (strlen($_POST['pin'])==4 && $_POST['value']>0 && strftime('%Y-%m',strtotime($_POST['date']))>=strftime('%Y-%m',time())) {
 		header('Content-Description: QR Code Image');
 		header('Content-Type: image/png');
@@ -10,6 +10,6 @@
 		header('Expires: 0');
 		header('Cache-Control: must-revalidate');
 		header('Pragma: public');
-		QRcode::png( 'AMDeposit:' . number_format($_POST['value'],2) . ':' . $code . ':' . $id);
+		QRcode::png( 'AMDeposit:' . number_format($_POST['value'],2) . ':' . $code . ':' . $hash);
 	} else echo 'ERROR';
 ?>
