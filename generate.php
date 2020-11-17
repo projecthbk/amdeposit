e7e21d7c81bab338a86486e4c8b1baecebc55753
ProjectHBK <34718421+projecthbk@users.noreply.github.com>
2020-11-18 00:51:29 +0100

    Update generate.php

--- generate.php
+++ generate.php
@@ -4,9 +4,9 @@
 	$hash = bin2hex(openssl_random_pseudo_bytes(12));
 	$code = hash('sha256',number_format($_POST['value'],2) . strftime('%Y-%m',strtotime($_POST['date'])) . $_POST['pin'] . $hash . $secret);
 	if (strlen($_POST['pin'])==4 && $_POST['value']>0 && strftime('%Y-%m',strtotime($_POST['date']))>=strftime('%Y-%m',time())) {
-		header('Content-Description: QR Code Image');
+		header('Content-Description: AMDeposit QR Code Image');
 		header('Content-Type: image/png');
-		header('Content-Disposition: attachment; filename="QRCode.png"');
+		header('Content-Disposition: attachment; filename="AMDeposit.png"');
 		header('Expires: 0');
 		header('Cache-Control: must-revalidate');
 		header('Pragma: public');
