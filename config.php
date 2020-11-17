d1808925e166df2e96b3e5e5db6f0bc18e3153b1
ProjectHBK <34718421+projecthbk@users.noreply.github.com>
2020-11-17 23:18:18 +0100

    Update config.php

--- config.php
+++ config.php
@@ -3,6 +3,4 @@
 	$path = '/home/pi/amdeposit'; 
 	//security protection string (individual to your deposit)
 	$secret = 'secret';
-	//CRON user to do monthly purge of temporary files
-	$user = 'pi';
 ?>
