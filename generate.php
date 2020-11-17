4a042d30b5994b46c43b7af3f69bcbf43730c92a
ProjectHBK <34718421+projecthbk@users.noreply.github.com>
2020-11-17 23:54:45 +0100

    Update generate.php

--- generate.php
+++ generate.php
@@ -3,7 +3,7 @@
 	require_once('phpqrcode/qrlib.php');
 	$id = bin2hex(openssl_random_pseudo_bytes(12));
 	$code = hash('sha256',number_format($_POST['value'],2) . strftime('%Y-%m',strtotime($_POST['date'])) . $_POST['pin'] . $id . $secret);
-	if (strlen($_POST['pin'])==4 && $_POST['value']>0 && $_POST['date']!='') {
+	if (strlen($_POST['pin'])==4 && $_POST['value']>0 && strftime('%Y-%m',strtotime($_POST['date']))>=strftime('%Y-%m',time())) {
 		header('Content-Description: QR Code Image');
 		header('Content-Type: image/png');
 		header('Content-Disposition: attachment; filename="QRCode.png"');
