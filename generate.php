71795231a70314b39d97e02fb840dfb5e6aa3de7
ProjectHBK <34718421+projecthbk@users.noreply.github.com>
2020-11-17 23:35:39 +0100

    Update generate.php

--- generate.php
+++ generate.php
@@ -1,17 +1,18 @@
 <?php
 	require_once('config.php');
 	require_once('phpqrcode/qrlib.php');
-	$codes = unserialize(file_get_contents($path . '/AMDnew'));
-	$code = hash('sha256',number_format($_POST['value'],2) . strftime('%Y-%m',strtotime($_POST['date'])) . $_POST['pin'] . $secret);
-	if (!in_array($code,$codes) && strlen($_POST['pin'])==4 && $_POST['value']>0 && $_POST['date']!='') {
-		$codes[]=$code;
-		file_put_contents($path . '/AMDnew',serialize($codes));
+	$id = rand(100000,999999);
+	$codes = unserialize(file_get_contents($path . '/AMDcreate'));
+	$code = hash('sha256',number_format($_POST['value'],2) . strftime('%Y-%m',strtotime($_POST['date'])) . $_POST['pin'] . $id . $secret);
+	if (!in_array($code . ':' . $id,$codes) && strlen($_POST['pin'])==4 && $_POST['value']>0 && $_POST['date']!='') {
+		$codes[]=$code . ':' . $id;
+		file_put_contents($path . '/AMDcreate',serialize($codes));
 		header('Content-Description: QR Code Image');
 		header('Content-Type: image/png');
 		header('Content-Disposition: attachment; filename="QRCode.png"');
 		header('Expires: 0');
 		header('Cache-Control: must-revalidate');
 		header('Pragma: public');
-		QRcode::png( 'AMDeposit:' . number_format($_POST['value'],2) . ':' . $code );
+		QRcode::png( 'AMDeposit:' . number_format($_POST['value'],2) . ':' . $code . ':' . $id);
 	} else echo 'ERROR';
 ?>
