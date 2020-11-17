4b63b202079c83366a829c712c3cc56c91ec717d
ProjectHBK <34718421+projecthbk@users.noreply.github.com>
2020-11-18 00:32:19 +0100

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 	//path to secure folder (not public on web server)
-	$path = '/home/pi/74d4ddea0d225d5'; 
+	$path = '/home/pi/74d4ddea0d225d5a'; 
 	//security protection string (individual to your deposit)
 	$secret = '9dad538309d45362';
 ?>
