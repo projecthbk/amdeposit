1ae3390cbf18dcff0c9178f0e5dfd814d50fb38d
ProjectHBK <34718421+projecthbk@users.noreply.github.com>
2020-11-17 23:43:31 +0100

    Update generate.php

--- generate.php
+++ generate.php
@@ -1,12 +1,9 @@
 <?php
 	require_once('config.php');
 	require_once('phpqrcode/qrlib.php');
-	$id = rand(100000,999999);
-	$codes = unserialize(file_get_contents($path . '/AMDcreate'));
+	$id = bin2hex(openssl_random_pseudo_bytes(12));
 	$code = hash('sha256',number_format($_POST['value'],2) . strftime('%Y-%m',strtotime($_POST['date'])) . $_POST['pin'] . $id . $secret);
-	if (!in_array($code . ':' . $id,$codes) && strlen($_POST['pin'])==4 && $_POST['value']>0 && $_POST['date']!='') {
-		$codes[]=$code . ':' . $id;
-		file_put_contents($path . '/AMDcreate',serialize($codes));
+	if (strlen($_POST['pin'])==4 && $_POST['value']>0 && $_POST['date']!='') {
 		header('Content-Description: QR Code Image');
 		header('Content-Type: image/png');
 		header('Content-Disposition: attachment; filename="QRCode.png"');
