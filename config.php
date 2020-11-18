f4821d2b444621e5c82b452a911deb05e4eb5523
ProjectHBK <34718421+projecthbk@users.noreply.github.com>
2020-11-18 01:23:03 +0100

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 	//path to secure folder (not public on web server) preferably with non readable but executable parent folder (chmod 711)
 	$path = '/home/pi/74d4ddea0d225d5a'; 
-	//security protection string (individual to your deposit) protected from any changes during whole system lifetime
+	//security protection string (individual to your deposit system) protected from any changes during whole system lifetime
 	$secret = 'bb88a4dd51973147d62225718034c9d6';
 ?>
