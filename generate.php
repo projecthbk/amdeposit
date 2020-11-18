f5372aca8bf259aaa9416172c55ebf6afe8ab3d5
ProjectHBK <34718421+projecthbk@users.noreply.github.com>
2020-11-18 06:25:34 +0100

    Update generate.php

--- generate.php
+++ generate.php
@@ -1,8 +1,8 @@
 <?php
 	require_once('config.php');
 	require_once('phpqrcode/qrlib.php');
-	$hash = bin2hex(openssl_random_pseudo_bytes(12));
-	$code = hash('sha256',number_format($_POST['value'],2) . strftime('%Y-%m',strtotime($_POST['date'])) . $_POST['pin'] . $hash . $secret);
+	$hash = base64_encode(openssl_random_pseudo_bytes(12));
+	$code = base64_encode(hash('sha256',number_format($_POST['value'],2) . strftime('%Y-%m',strtotime($_POST['date'])) . $_POST['pin'] . $hash . $secret,true));
 	if (strlen($_POST['pin'])==4 && $_POST['value']>0 && strftime('%Y-%m',strtotime($_POST['date']))>=strftime('%Y-%m',time())) {
 		header('Content-Description: AMDeposit QR Code Image');
 		header('Content-Type: image/png');
